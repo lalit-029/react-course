b85debb88a4ee316a4c516279f25ce5b4a29b635
Vinay Mehta <vinay@legitimate.io>
2020-10-29 19:16:59 +0530

    added home page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+import App from './App';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <div>
+    <App />
+  </div>,
   document.getElementById('root')
 );
