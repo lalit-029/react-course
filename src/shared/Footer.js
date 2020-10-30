412314717077c4a50af6fa701bb43b58a7155471
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-10-30 13:07:16 +0530

    fixed ui for footer

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import Navbar from 'react-bootstrap/Navbar';
 import {Container, Row,  Col} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faFacebookF, faTwitter, faLinkedinIn, faYoutube} from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
@@ -8,10 +9,15 @@ function Footer() {
         <Container className="p-0">
             <Row>
                 <Col xs="12" lg="6" className="py-4">
-                    <p className="mb-0">&copy; Lalit Kumar, 2020</p>
+                    <span className="mb-0">&copy; Lalit Kumar, 2020</span>
                 </Col>
                 <Col xs="12" lg="6" className="py-4">
-                    
+                    <div className="font-row d-flex justify-content-end">
+                        <a className="px-2"><FontAwesomeIcon icon={faFacebookF} /></a>
+                        <a className="px-2"><FontAwesomeIcon icon={faTwitter} /></a>
+                        <a className="px-2"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                        <a className="px-2"><FontAwesomeIcon icon={faYoutube} /></a>
+                    </div>
                 </Col>
             </Row>
         </Container>
