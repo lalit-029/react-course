0811dc62f8f8eed94e08f09b99991b4da5ce6de6
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-10-30 18:48:56 +0530

    fixed ui for home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.scss'; 
 import Header from './shared/Header';
 import Footer from './shared/Footer';
 import Home from './home/Home';
+import About from "./main/About";
+import Contact from "./main/Contact";
+import Services from "./main/Services";
+import Resume from "./main/Resume";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component = {Header}/>
+          <Route exact path='/home' Component={Home} />
+          <Route exact path='/about' Component={About} />
+          <Route exact path='/contact' Component={Contact} />
+          <Route exact path='/service' Component={Services} />
+          <Route exact path='/resume' Component={Resume} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
