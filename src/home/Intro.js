31b7bfd202121dba1471b37b39e7ad47247685f9
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-10-30 15:34:01 +0530

    added intro content in home page

---
+++ src/home/Intro.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'; 
+import {Col} from 'react-bootstrap';
+
+function Intro() {
+    return (
+        <Col xs="12" lg="12">
+            <h1 className="banner-title">Hello, my name is lalit kumar. I'm a <span>UI Designer</span> with 1+ years of experience in the digital world.</h1>
+        </Col>
+    );
+}
+export default Intro;
