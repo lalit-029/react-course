3f479429c7be1665c46699528fdcb54a1fd698f2
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-10-30 15:38:28 +0530

    added skills content in home page

--- src/home/Skills.js
+++ src/home/Skills.js
@@ -4,8 +4,28 @@ import {Row, Col} from 'react-bootstrap';
 function Skills() {
     return (
         <Row className="p-5">
-            <Col xs="12" lg={{ span: 6, offset: 3 }}>
-                
+            <Col xs="12" lg="6">
+
+            </Col>
+            <Col xs="12" lg="6">
+                <div className="experience-list">
+                    <div className="experience-year">
+                        <h6>Apr - 2019</h6>
+                    </div>
+                    <div className="experience-post">
+                        <h5>UI Designer</h5>
+                        <small>BERYL SYSTEMS PVT. LTD.</small>
+                    </div>
+                </div>
+                <div className="experience-list">
+                    <div className="experience-year">
+                        <h6>Jan - 2019</h6>
+                    </div>
+                    <div className="experience-post">
+                        <h5>Web Designer</h5>
+                        <small>Ducat</small>
+                    </div>
+                </div>
             </Col>
         </Row>
     );
