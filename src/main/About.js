7f626bf1c147e59ead9a3105f52d0c20d441af99
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-10-31 15:48:34 +0530

    fixed ui for about page

---
+++ src/main/About.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+import {Row, Col, Container} from 'react-bootstrap';
+import Skills from '../home/Skills';
+import Intro from '../home/Intro';
+function About() {
+    return (
+        <>
+            <Container className="banner-container">
+                <Row>
+                    <Intro />
+                </Row>
+            </Container>
+            <Container className="p-0 py-5">
+                <h2 className="text-center mb-0 pb-5">Skills & Experiences</h2>
+                <Skills />
+            </Container>
+        </>
+    );
+}
+export default About;
