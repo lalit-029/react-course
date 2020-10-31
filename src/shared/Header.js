b87ff341a06d2a677b532bf2e038bb90087a84da
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-10-31 15:27:20 +0530

    added links in header

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -7,14 +7,14 @@ function Header() {
         <Container fluid className="p-0">
             <Container className="p-0">
                 <Navbar collapseOnSelect expand="lg" className="p-3">
-                <Navbar.Brand href="#home" className="p-lg-0">Lalit <span>Kumar</span></Navbar.Brand>
+                <Navbar.Brand href="/" className="p-lg-0">Lalit <span>Kumar</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                    <Nav.Link className="text-xs-left" href="#">Services</Nav.Link>
-                    <Nav.Link className="text-xs-left" href="#">Resume</Nav.Link>
-                    <Nav.Link className="text-xs-left" href="#">About Me</Nav.Link>
-                    <Nav.Link className="text-xs-left" href="#">Contact Us</Nav.Link>
+                        <Nav.Link className="text-xs-left" href="/service">Services</Nav.Link>
+                        <Nav.Link className="text-xs-left" href="/resume">Resume</Nav.Link>
+                        <Nav.Link className="text-xs-left" href="/about">About Me</Nav.Link>
+                        <Nav.Link className="text-xs-left" href="/contact">Contact Us</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
