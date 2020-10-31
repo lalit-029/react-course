20b80adb4d5f43596c7af4b98ba69446bec90fda
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-10-31 15:24:09 +0530

    completed ui for service page

--- src/main/Services.js
+++ src/main/Services.js
@@ -1,25 +1,32 @@
 import React from 'react'; 
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Container} from 'react-bootstrap';
 
 function Services() {
     return (
-        <Row className="work-row">
-            <Col xs="12" lg="4" className="p-5 work-card">
-                <img src="animation.png" className="mb-5" alt="services"/>
-                <h4 className="mb-4">Animation</h4>
-                <p>I blew a little magic to make static designs work without coding. Yah! It's Animation.</p>
-            </Col>
-            <Col xs="12" lg="4" className="p-5 work-card">
-                <img src="interaction.jpg" className="mb-5 interaction" alt="services"/>
-                <h4 className="mb-4">Interaction</h4>
-                <p>Optimized UI/UX for digital products like desktop, mobile, etc. Help business growth more.</p>
-            </Col>
-            <Col xs="12" lg="4" className="p-5 work-card">
-                <img src="illustration.jpg" className="mb-5 illustration" alt="services"/>
-                <h4 className="mb-4">Illustration</h4>
-                <p>Design things abstract become visible with a look impressive.</p>
-            </Col>
-        </Row>
+        <Container className="px-0">
+            <Row>
+                <Col xs="12" lg={{ span: 6, offset: 3 }} className="py-5">
+                    <h2 className="text-center mb-0">I Know That <span>Good Design</span> Meant <span>Good Business</span></h2>
+                </Col>
+            </Row>
+            <Row className="work-row">
+                <Col xs="12" lg="4" className="p-5 work-card">
+                    <img src="animation.png" className="mb-5" alt="services"/>
+                    <h4 className="mb-4">Animation</h4>
+                    <p>I blew a little magic to make static designs work without coding. Yah! It's Animation.</p>
+                </Col>
+                <Col xs="12" lg="4" className="p-5 work-card">
+                    <img src="interaction.jpg" className="mb-5 interaction" alt="services"/>
+                    <h4 className="mb-4">Interaction</h4>
+                    <p>Optimized UI/UX for digital products like desktop, mobile, etc. Help business growth more.</p>
+                </Col>
+                <Col xs="12" lg="4" className="p-5 work-card">
+                    <img src="illustration.jpg" className="mb-5 illustration" alt="services"/>
+                    <h4 className="mb-4">Illustration</h4>
+                    <p>Design things abstract become visible with a look impressive.</p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 export default Services;
