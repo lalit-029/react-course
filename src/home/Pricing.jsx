14884e97f390ea4beb04a2e0612ee99b9eca0dd7
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-02 18:41:48 +0530

    fixed ui for pricing in home page

---
+++ src/home/Pricing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Row, Col, Button, Card} from "react-bootstrap";
+function PricingCard(props) {
+    return(
+        <>
+            <Card>
+                <Card.Img variant="top" src={props.imagesrc} alt="pricing-image" />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>
+                    {props.description}
+                    </Card.Text>
+                    <Button variant="primary" className="w-100">Book now - {props.price}</Button>
+                </Card.Body>
+            </Card>
+                
+        </>
+    );
+}
+export default PricingCard;
