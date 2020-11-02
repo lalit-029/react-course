1d1a8ff12b1e1e581527932811e7cd967d994160
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-02 12:01:02 +0530

    printed current date and time in footer

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -3,15 +3,20 @@ import {Container, Row,  Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { faFacebookF, faTwitter, faLinkedinIn, faYoutube} from '@fortawesome/free-brands-svg-icons';
 
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
 function Footer() {
   return (
     <Container fluid className="p-0 footer-container">
         <Container className="p-0">
             <Row>
-                <Col xs="12" lg="6" className="py-4">
+                <Col xs="12" lg="4" className="py-4">
                     <span className="mb-0">&copy; Lalit Kumar, 2020</span>
                 </Col>
-                <Col xs="12" lg="6" className="py-4">
+                <Col xs="12" lg="4" className="py-4 text-center">
+                    <span className="mb-0">{currentDate}&nbsp;&nbsp;{currentTime}</span>
+                </Col>
+                <Col xs="12" lg="4" className="py-4">
                     <div className="font-row d-flex justify-content-end">
                         <a className="px-2"><FontAwesomeIcon icon={faFacebookF} /></a>
                         <a className="px-2"><FontAwesomeIcon icon={faTwitter} /></a>
