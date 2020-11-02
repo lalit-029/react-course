c9e66b9b60f6ee4cf237ae726ea52419b6fdad3b
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-02 12:27:43 +0530

    change greeting in intro section with time

--- src/home/Intro.js
+++ src/home/Intro.js
@@ -1,10 +1,23 @@
 import React from 'react'; 
 import {Col} from 'react-bootstrap';
 
+let currDate = new Date(2020, 11, 2, 19);
+currDate = currDate.getHours();
+let greeting = "";
+if ( currDate >= 1 && currDate < 12 ) {
+    greeting = "Good Morning!!";
+} else if ( currDate >= 12 && currDate < 16 ) {
+    greeting = "Good Afternoon!!";
+} else if ( currDate >= 16 && currDate < 19) {
+    greeting = "Good Evening!!";
+} else{
+    greeting = "";
+}
+
 function Intro() {
     return (
         <Col xs="12" lg="12">
-            <h1 className="banner-title">Hello, my name is lalit kumar. I'm a <span>UI Designer</span> with 1+ years of experience in the digital world.</h1>
+            <h1 className="banner-title">{greeting} Hello, my name is lalit kumar. I'm a <span>UI Designer</span> with 1+ years of experience in the digital world.</h1>
         </Col>
     );
 }
