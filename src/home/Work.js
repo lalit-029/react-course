079804c7e4954ba819d7ec05aa42ee414b3afd73
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-04 18:20:18 +0530

    added props for services section

--- src/home/Work.js
+++ src/home/Work.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react'; 
 import {Row, Col} from 'react-bootstrap';
 
-const Work = () => {
+const Work = (props) => {
     return (
-        <Row className="work-row">
-            <Col xs="12" lg="4" className="p-5 work-card">
-                <img src="animation.png" className="mb-5" alt="work" />
-                <h4 className="mb-4">Animation</h4>
-                <p>I blew a little magic to make static designs work without coding. Yah! It's Animation.</p>
-            </Col>
-            <Col xs="12" lg="4" className="p-5 work-card">
-                <img src="interaction.jpg" className="mb-5 interaction" alt="work" />
-                <h4 className="mb-4">Interaction</h4>
-                <p>Optimized UI/UX for digital products like desktop, mobile, etc. Help business growth more.</p>
-            </Col>
-            <Col xs="12" lg="4" className="p-5 work-card">
-                <img src="illustration.jpg" className="mb-5 illustration" alt="work" />
-                <h4 className="mb-4">Illustration</h4>
-                <p>Design things abstract become visible with a look impressive.</p>
-            </Col>
-        </Row>
+        <Col xs="12" lg="4" className="p-5 work-card">
+            <img src={props.imagesrc} className={`mb-5 ${props.className}`} alt="work" />
+            <h4 className="mb-4">{props.title}</h4>
+            <p>{props.description}</p>
+        </Col>
     );
 }
 export default Work;
