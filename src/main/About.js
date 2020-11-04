bc3190b919ff65d5de6b25a5ef27c23b88ef6835
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-04 13:10:45 +0530

    function component changed to class component

--- src/main/About.js
+++ src/main/About.js
@@ -1,20 +1,22 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import {Row, Col, Container} from 'react-bootstrap';
 import Skills from '../home/Skills';
 import Intro from '../home/Intro';
-function About() {
-    return (
-        <>
-            <Container className="banner-container">
-                <Row>
-                    <Intro />
-                </Row>
-            </Container>
-            <Container className="p-0 py-5">
-                <h2 className="text-center mb-0 pb-5">Skills & Experiences</h2>
-                <Skills />
-            </Container>
-        </>
-    );
+class About extends Component {
+    render(){
+        return (
+            <>
+                <Container className="banner-container">
+                    <Row>
+                        <Intro />
+                    </Row>
+                </Container>
+                <Container className="p-0 py-5">
+                    <h2 className="text-center mb-0 pb-5">Skills & Experiences</h2>
+                    <Skills />
+                </Container>
+            </>
+        );
+    }
 }
 export default About;
