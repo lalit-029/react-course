91cb5b7f83a5f647509b14681121ae11865bb560
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-04 15:26:40 +0530

    changed class method to const function method

--- src/main/About.js
+++ src/main/About.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import {Row, Col, Container} from 'react-bootstrap';
 import Skills from '../home/Skills';
 import Intro from '../home/Intro';
-class About extends Component {
-    render(){
-        return (
-            <>
-                <Container className="banner-container">
-                    <Row>
-                        <Intro />
-                    </Row>
-                </Container>
-                <Container className="p-0 py-5">
-                    <h2 className="text-center mb-0 pb-5">Skills & Experiences</h2>
-                    <Skills />
-                </Container>
-            </>
-        );
-    }
+const About = () => {
+    return (
+        <>
+            <Container className="banner-container">
+                <Row>
+                    <Intro />
+                </Row>
+            </Container>
+            <Container className="p-0 py-5">
+                <h2 className="text-center mb-0 pb-5">Skills & Experiences</h2>
+                <Skills />
+            </Container>
+        </>
+    );
 }
 export default About;
