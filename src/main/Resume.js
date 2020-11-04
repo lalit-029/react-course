2849f9bb98fb0ea7d45c61d4feed77fdb07bad8a
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-04 12:50:28 +0530

    function component changed to class component

--- src/main/Resume.js
+++ src/main/Resume.js
@@ -1,17 +1,19 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import {Row, Button, Col, Container} from 'react-bootstrap';
 
-function Resume() {
-    return (
-        <Container className="px-0 resume-container">
-            <Row>
-                <Col xs="12" lg={{ span: 4, offset: 4 }} className="text-center">
-                    <h3>Download my Curriculum Vitae</h3>
-                    <Button variant="primary" className="my-2">Download Now!</Button>
-                    <p className="mt-5"><small>For any further queries please contact me at <span>lalitkr.uiux@gmail.com</span></small></p>
-                </Col>
-            </Row>
-        </Container>
-    );
+class Resume extends Component {
+    render(){
+        return (
+            <Container className="px-0 resume-container">
+                <Row>
+                    <Col xs="12" lg={{ span: 4, offset: 4 }} className="text-center">
+                        <h3>Download my Curriculum Vitae</h3>
+                        <Button variant="primary" className="my-2">Download Now!</Button>
+                        <p className="mt-5"><small>For any further queries please contact me at <span>lalitkr.uiux@gmail.com</span></small></p>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 export default Resume;
