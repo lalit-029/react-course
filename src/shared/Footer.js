f8ceaf3369a705d5d04156af9b6859b74c1594af
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-04 13:12:29 +0530

    function component changed to class component

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Container, Row,  Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { faFacebookF, faTwitter, faLinkedinIn, faYoutube} from '@fortawesome/free-brands-svg-icons';
 
 const currentDate = new Date().toLocaleDateString();
 const currentTime = new Date().toLocaleTimeString();
-function Footer() {
-  return (
-    <Container fluid className="p-0 footer-container">
-        <Container className="p-0">
-            <Row>
-                <Col xs="12" lg="4" className="py-4">
-                    <span className="mb-0">&copy; Lalit Kumar, 2020</span>
-                </Col>
-                <Col xs="12" lg="4" className="py-4 text-center">
-                    <span className="mb-0">{currentDate}&nbsp;&nbsp;{currentTime}</span>
-                </Col>
-                <Col xs="12" lg="4" className="py-4">
-                    <div className="font-row d-flex justify-content-end">
-                        <a className="px-2"><FontAwesomeIcon icon={faFacebookF} /></a>
-                        <a className="px-2"><FontAwesomeIcon icon={faTwitter} /></a>
-                        <a className="px-2"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-                        <a className="px-2"><FontAwesomeIcon icon={faYoutube} /></a>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    </Container>    
-  );
+class Footer extends Component {
+    render(){
+        return (
+            <Container fluid className="p-0 footer-container">
+                <Container className="p-0">
+                    <Row>
+                        <Col xs="12" lg="4" className="py-4">
+                            <span className="mb-0">&copy; Lalit Kumar, 2020</span>
+                        </Col>
+                        <Col xs="12" lg="4" className="py-4 text-center">
+                            <span className="mb-0">{currentDate}&nbsp;&nbsp;{currentTime}</span>
+                        </Col>
+                        <Col xs="12" lg="4" className="py-4">
+                            <div className="font-row d-flex justify-content-end">
+                                <a className="px-2"><FontAwesomeIcon icon={faFacebookF} /></a>
+                                <a className="px-2"><FontAwesomeIcon icon={faTwitter} /></a>
+                                <a className="px-2"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                                <a className="px-2"><FontAwesomeIcon icon={faYoutube} /></a>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>    
+        );
+    }
 }
 export default Footer;
