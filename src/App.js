e49d7bdd88129047ec8250c5336d83a284ce0aaa
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-04 12:56:53 +0530

    function component changed to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.scss'; 
 import Header from './shared/Header';
@@ -10,32 +10,34 @@ import Services from "./main/Services";
 import Resume from "./main/Resume";
 
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route exact path='/contact'>
-            <Contact />
-          </Route>
-          <Route exact path='/service'>
-            <Services />
-          </Route>
-          <Route exact path='/resume'>
-            <Resume />
-          </Route>
-        </Switch>
-      </Router>
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/contact'>
+              <Contact />
+            </Route>
+            <Route exact path='/service'>
+              <Services />
+            </Route>
+            <Route exact path='/resume'>
+              <Resume />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
