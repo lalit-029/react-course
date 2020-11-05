9644c5db6372260c1cbce6e9820b943b0c9617d2
Lalit Kumar <Lalit.kumar@berylsystems.com>
2020-11-05 13:54:08 +0530

    slotgame completed using if and else in props

--- src/main/Game.js
+++ src/main/Game.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
-
-const Game = () => {
+import {Row, Col} from "react-bootstrap";
+import Slotgame from './Slotgame';
+const Game = (props) => {
     return(
-        <>
-            <h2 className="text-center my-5">Welcome to <span style={{fontWeight:"Bold", textTransform: "Uppercase"}}>slot machine game</span></h2>
-        </>
+        <div className="py-5">
+            <h2 className="text-center my-5"> 🎰 Welcome to <span style={{fontWeight:"Bold", textTransform: "Uppercase"}}>slot machine game 🎰 </span></h2>
+            <Row className="my-5 mx-0">
+                <Col xs="12" lg={{span: 4, offset: 4}} className="text-center">
+                    <div className="slot-machine">
+                        <Slotgame x="😃" y="😃" z="😃" />
+                        <Slotgame x="😃" y="😃" z="😺" />
+                        <Slotgame x="😃" y="😈" z="😃" />
+                        <Slotgame x="❄️" y="❄️" z="❄️" />
+                    </div>
+                </Col>
+            </Row>
+        </div>
     )
 };
 
